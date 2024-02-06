1944216d83e7ae1f7d1e99b371e4b27e025f3136
RISHIKESHdev <rishikeshhappydeveloper@gmail.com>
2024-02-06 15:00:51 +0530

    Class Diagram v1

--- com/bms/transaction/cash/Cash.java
+++ com/bms/transaction/cash/Cash.java
@@ -6,4 +6,16 @@ import java.util.List;
 
 public class Cash implements PaymentMode {
     private List<Currency> currencyList;
+
+    public Cash(List<Currency> currencyList) {
+        this.currencyList = currencyList;
+    }
+
+    public List<Currency> getCurrencyList() {
+        return currencyList;
+    }
+
+    public void addCurrencyList(Currency currency) {
+        this.currencyList.add(currency);
+    }
 }
--- com/bms/transaction/cash/Currency.java
+++ com/bms/transaction/cash/Currency.java
@@ -3,4 +3,25 @@ package com.bms.transaction.cash;
 public class Currency {
     private double INRValue;
     private int count;
+
+    public Currency(double INRValue, int count) {
+        this.INRValue = INRValue;
+        this.count = count;
+    }
+
+    public double getINRValue() {
+        return INRValue;
+    }
+
+    public void setINRValue(double INRValue) {
+        this.INRValue = INRValue;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
